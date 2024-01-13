6d991693a7e4aa4adf70c7ac717f5b80db30e70a
sdAswaathkrishna <aswathkrishna0922@gmail.com>
2024-01-13 20:31:06 +0530

    modified

--- 1347. Minimum Number of Steps to Make Two Strings Anagram.py
+++ 1347. Minimum Number of Steps to Make Two Strings Anagram.py
@@ -6,11 +6,11 @@ def minSteps(s, t):
     for i in set_s:
         if t.count(i) < s.count(i):
             min_steps += s.count(i) - t.count(i)
-            print(i, min_steps)
+            #print(i, min_steps)
             
     return min_steps
 
 output = minSteps("aaa", "bbb")
 print(output)
 
-        
+#find the difference in frequencies of letters in s and t and add it to min_steps      
