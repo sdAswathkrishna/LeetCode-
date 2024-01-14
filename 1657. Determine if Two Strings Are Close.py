c22475fe63c1036ebd8808bbaa1fac6211329b4a
sdAswaathkrishna <aswathkrishna0922@gmail.com>
2024-01-14 22:16:49 +0530

    7th Done

---
+++ 1657. Determine if Two Strings Are Close.py
@@ -0,0 +1,22 @@
+def closeStrings(word1, word2):
+    if word1 == word2 :
+        return True
+    if len(word1) != len(word2):
+        return False
+    a = {i: word1.count(i) for i in set(word1)}
+    b = {i: word2.count(i) for i in set(word2)}
+    for i in a:
+        if i not in b:
+            return False
+    a = list(a.values())
+    b = list(b.values())
+    a.sort()
+    b.sort()
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            return False
+    return True
+    
+output = closeStrings("a", "aa")
+print(output)
+        
